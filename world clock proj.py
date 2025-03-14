7f5a0b44d09c297cd184c4d8d7c1c2ee617e8188
Danniejerome <128336818+Danniejerome@users.noreply.github.com>
2025-03-14 20:02:55 +0530

    Delete world clock proj.py

--- world clock proj.py
+++
@@ -1,7 +0,0 @@
-import pytz
-from datetime import datetime
-# time_zones = pytz.all_timezones
-# print(time_zones)
-country_name = pytz.timezone('Asia/Delhi')
-country_time = datetime.now(country_name)
-print(country_time.strftime("Date is %d-%m-%y and time is %H:%M:%S"))
